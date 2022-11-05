3798aabfcc6f78645bf3d7255dfd6c336cd497f0
Hong Liang Teoh <liangtl@amazon.com>
2022-11-05 17:19:41 +0000

    [FLINK-29909][Connectors/DynamoDB] Update package names to follow convention of other AWS connectors

---
+++ flink-connector-dynamodb/src/main/java/org/apache/flink/connector/dynamodb/util/PrimaryKeyBuilder.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.dynamodb.util;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.connector.dynamodb.sink.InvalidConfigurationException;
+import org.apache.flink.connector.dynamodb.sink.InvalidRequestException;
+import org.apache.flink.util.CollectionUtil;
+
+import org.apache.commons.lang3.StringUtils;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.WriteRequest;
+
+import java.util.List;
+import java.util.Map;
+
+/** Helper to construct primary(composite) key for a DynamoDB request. */
+@Internal
+public class PrimaryKeyBuilder {
+
+    private static final String DELIMITER = "$";
+
+    private final List<String> partitionKeys;
+
+    public PrimaryKeyBuilder(List<String> partitionKeys) {
+        if (CollectionUtil.isNullOrEmpty(partitionKeys)) {
+            throw new InvalidConfigurationException(
+                    "Can not construct partition key as overwriteByPartitionKeys configuration not provided");
+        }
+
+        this.partitionKeys = partitionKeys;
+    }
+
+    public String build(WriteRequest request) {
+
+        Map<String, AttributeValue> requestItems = getRequestItems(request);
+
+        StringBuilder builder = new StringBuilder();
+        for (String keyName : partitionKeys) {
+            AttributeValue keyAttribute = requestItems.get(keyName);
+
+            if (keyAttribute == null) {
+                throw new InvalidRequestException(
+                        "Request "
+                                + request.toString()
+                                + " does not contain partition key "
+                                + keyName
+                                + ".");
+            }
+
+            String keyValue = getKeyValue(keyAttribute);
+
+            if (StringUtils.isBlank(keyValue)) {
+                throw new InvalidRequestException(
+                        "Partition key or sort key attributes require non-empty values. Request "
+                                + request.toString()
+                                + " contains empty key "
+                                + keyName
+                                + ".");
+            }
+
+            builder.append(keyValue).append(DELIMITER);
+        }
+
+        return builder.toString();
+    }
+
+    /**
+     * Returns string value of a partition key attribute. Each primary key attribute must be defined
+     * as type String, Number, or binary as per DynamoDB specification.
+     */
+    private static String getKeyValue(AttributeValue value) {
+        StringBuilder builder = new StringBuilder();
+
+        if (value.n() != null) {
+            builder.append(value.n());
+        }
+
+        if (value.s() != null) {
+            builder.append(value.s());
+        }
+
+        if (value.b() != null) {
+            builder.append(value.b().asUtf8String());
+        }
+
+        return builder.toString();
+    }
+
+    private static Map<String, AttributeValue> getRequestItems(WriteRequest request) {
+        if (request.putRequest() != null) {
+            if (request.putRequest().hasItem()) {
+                return request.putRequest().item();
+            } else {
+                throw new InvalidRequestException(
+                        "PutItemRequest " + request.toString() + " does not contain request items");
+            }
+        } else if (request.deleteRequest() != null) {
+            if (request.deleteRequest().hasKey()) {
+                return request.deleteRequest().key();
+            } else {
+                throw new InvalidRequestException(
+                        "DeleteItemRequest "
+                                + request.toString()
+                                + " does not contain request key");
+            }
+        } else {
+            throw new InvalidRequestException("Empty write request" + request.toString());
+        }
+    }
+}
---
+++ flink-connector-dynamodb/src/test/java/org/apache/flink/connector/dynamodb/util/DynamoDbSerializationUtilTest.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.dynamodb.util;
+
+import org.apache.flink.connector.dynamodb.sink.DynamoDbWriteRequest;
+import org.apache.flink.connector.dynamodb.sink.DynamoDbWriteRequestType;
+
+import org.apache.flink.shaded.guava30.com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Tests for {@link DynamoDbSerializationUtil}. */
+public class DynamoDbSerializationUtilTest {
+
+    @Test
+    public void testPutItemSerializeDeserialize() throws IOException {
+        final Map<String, AttributeValue> item = new HashMap<>();
+        item.put("string", AttributeValue.builder().s("string").build());
+        item.put("number", AttributeValue.builder().s("123.4212").build());
+        item.put(
+                "binary",
+                AttributeValue.builder().b(SdkBytes.fromByteArray(new byte[] {1, 2, 3})).build());
+        item.put("null", AttributeValue.builder().nul(true).build());
+        item.put("boolean", AttributeValue.builder().bool(true).build());
+        item.put("stringSet", AttributeValue.builder().ss("s1", "s2").build());
+        item.put("numberSet", AttributeValue.builder().ns("123.1231", "123123").build());
+        item.put(
+                "binarySet",
+                AttributeValue.builder()
+                        .bs(
+                                SdkBytes.fromByteArray(new byte[] {1, 2, 3}),
+                                SdkBytes.fromByteArray(new byte[] {4, 5, 6}))
+                        .build());
+        item.put(
+                "list",
+                AttributeValue.builder()
+                        .l(
+                                AttributeValue.builder().s("s1").build(),
+                                AttributeValue.builder().s("s2").build())
+                        .build());
+        item.put(
+                "map",
+                AttributeValue.builder()
+                        .m(
+                                ImmutableMap.of(
+                                        "key1", AttributeValue.builder().s("string").build(),
+                                        "key2", AttributeValue.builder().n("12345").build(),
+                                        "binary",
+                                                AttributeValue.builder()
+                                                        .b(
+                                                                SdkBytes.fromByteArray(
+                                                                        new byte[] {1, 2, 3}))
+                                                        .build(),
+                                        "null", AttributeValue.builder().nul(true).build()))
+                        .build());
+        DynamoDbWriteRequest dynamoDbWriteRequest =
+                DynamoDbWriteRequest.builder()
+                        .setItem(item)
+                        .setType(DynamoDbWriteRequestType.PUT)
+                        .build();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(outputStream);
+        DynamoDbSerializationUtil.serializeWriteRequest(dynamoDbWriteRequest, out);
+        outputStream.close();
+        out.close();
+        InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        DynamoDbWriteRequest deserializedWriteRequest =
+                DynamoDbSerializationUtil.deserializeWriteRequest(dataInputStream);
+        inputStream.close();
+        assertThat(deserializedWriteRequest).isEqualTo(dynamoDbWriteRequest);
+    }
+
+    @Test
+    public void testDeleteItemSerializeDeserialize() throws IOException {
+        final Map<String, AttributeValue> key = new HashMap<>();
+        key.put("string", AttributeValue.builder().s("string").build());
+        key.put("number", AttributeValue.builder().s("123.4212").build());
+        DynamoDbWriteRequest dynamoDbWriteRequest =
+                DynamoDbWriteRequest.builder()
+                        .setItem(key)
+                        .setType(DynamoDbWriteRequestType.DELETE)
+                        .build();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(outputStream);
+        DynamoDbSerializationUtil.serializeWriteRequest(dynamoDbWriteRequest, out);
+        outputStream.close();
+        out.close();
+        InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        DynamoDbWriteRequest deserializedWriteRequest =
+                DynamoDbSerializationUtil.deserializeWriteRequest(dataInputStream);
+        inputStream.close();
+        assertThat(deserializedWriteRequest).isEqualTo(dynamoDbWriteRequest);
+    }
+}
