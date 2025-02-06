4a8831c97d140a703d4c2a4c3be74b470b3a8f15
Francisco <fmorillo@amazon.com>
2025-02-06 10:09:09 +0100

    adding factory

--- flink-catalog-aws-glue/src/main/java/com/amazonaws/services/msf/StreamingJob.java
+++ flink-catalog-aws-glue/src/main/java/com/amazonaws/services/msf/StreamingJob.java
@@ -24,56 +24,67 @@ public class StreamingJob {
         // Create the TableEnvironment using the provided execution environment and settings
         TableEnvironment tEnv = StreamTableEnvironment.create(
                 env, EnvironmentSettings.newInstance().build());
+//
+//        // Create a GlueCatalog for connecting Flink with AWS Glue
+//        Catalog glueCatalog = new GlueCatalog("glue_catalog", "default", "us-east-1");
+//
+//        // Register the Glue catalog in the TableEnvironment so that it can be accessed
+//        tEnv.registerCatalog("glue_catalog", glueCatalog);
+//
+//        // Set the registered Glue catalog as the default catalog
+//        tEnv.useCatalog("glue_catalog");
 
-        // Create a GlueCatalog for connecting Flink with AWS Glue
-        Catalog glueCatalog = new GlueCatalog("glue_catalog", "default", "us-east-1");
 
-        // Register the Glue catalog in the TableEnvironment so that it can be accessed
-        tEnv.registerCatalog("glue_catalog", glueCatalog);
-
-        // Set the registered Glue catalog as the default catalog
-        tEnv.useCatalog("glue_catalog");
-
-        // Example SQL statements to interact with the Glue catalog and databases
-        // Drop the 'test' database if it exists and create it
-        tEnv.executeSql("DROP DATABASE IF EXISTS test").print();
-        tEnv.executeSql("CREATE DATABASE IF NOT EXISTS test").print();
-
-        // Show the list of databases in the catalog
-        tEnv.executeSql("SHOW DATABASES").print();
-
-        // Switch to the 'test' database
-        tEnv.executeSql("USE test").print();
-
-        // Show the list of tables in the 'test' database
-        tEnv.executeSql("SHOW TABLES").print();
-
-        // Drop the 'fran' table if it exists and create it
-        tEnv.executeSql("DROP TABLE IF EXISTS kinesisTable").print();
-
-        // Create a new table 'fran' with specified schema and configuration
-        tEnv.executeSql("CREATE TABLE IF NOT EXISTS kinesisTable (" +
-                "  `user_id` STRING," +
-                "  `productName` STRING," +
-                "  `color` STRING," +
-                "  `department` STRING," +
-                "  `product` STRING," +
-                "  `campaign` STRING," +
-                "  `price` DOUBLE," +
-                "  `creationTimestamp` STRING" +
-                ")" +
-                "WITH (" +
-                "  'connector' = 'kinesis'," +
-                "  'stream.arn' = 'arn:aws:kinesis:us-east-1:xxxx:stream/input'," +
-                "  'aws.region' = 'us-east-1'," +
-                "  'source.init.position' = 'TRIM_HORIZON'," +
-                "  'format' = 'json'" +
-                ");").print();
-
-        // Show the list of tables in the 'test' database again after creating the new table
-        tEnv.executeSql("SHOW TABLES").print();
+        // Register the Glue catalog using SQL
+        tEnv.executeSql(
+                "CREATE CATALOG glue_catalog WITH (" +
+                        "'type' = 'glue', " +
+                        "'region' = 'us-east-1', " +
+                        "'default-database' = 'default' " +
+                        ")"
+        );
+//        tEnv.executeSql("USE CATALOG glue_catalog;").print();
+//
+//        // Example SQL statements to interact with the Glue catalog and databases
+//        // Drop the 'test' database if it exists and create it
+//        tEnv.executeSql("DROP DATABASE IF EXISTS test").print();
+//        tEnv.executeSql("CREATE DATABASE IF NOT EXISTS test").print();
+//
+//        // Show the list of databases in the catalog
+//        tEnv.executeSql("SHOW DATABASES").print();
+//
+//        // Switch to the 'test' database
+//        tEnv.executeSql("USE test").print();
+//
+//        // Show the list of tables in the 'test' database
+//        tEnv.executeSql("SHOW TABLES").print();
+//
+//        // Drop the 'fran' table if it exists and create it
+//        tEnv.executeSql("DROP TABLE IF EXISTS kinesisTable").print();
+//
+//        // Create a new table 'fran' with specified schema and configuration
+//        tEnv.executeSql("CREATE TABLE IF NOT EXISTS kinesisTable (" +
+//                "  `user_id` STRING," +
+//                "  `productName` STRING," +
+//                "  `color` STRING," +
+//                "  `department` STRING," +
+//                "  `product` STRING," +
+//                "  `campaign` STRING," +
+//                "  `price` DOUBLE," +
+//                "  `creationTimestamp` STRING" +
+//                ")" +
+//                "WITH (" +
+//                "  'connector' = 'kinesis'," +
+//                "  'stream.arn' = 'arn:aws:kinesis:us-east-1:xxxx:stream/input'," +
+//                "  'aws.region' = 'us-east-1'," +
+//                "  'source.init.position' = 'TRIM_HORIZON'," +
+//                "  'format' = 'json'" +
+//                ");").print();
+//
+//        // Show the list of tables in the 'test' database again after creating the new table
+//        tEnv.executeSql("SHOW TABLES").print();
 
         // Run a query to select all records from the 'fran' table and print the results
-        tEnv.executeSql("SELECT * FROM kinesisTable").print();
+//        tEnv.executeSql("SELECT * FROM kinesisTable").print();
     }
 }
